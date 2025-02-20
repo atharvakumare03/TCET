f3d7a64f91295331a23ca09063e0852140d1ed37
atharvakumare03 <157445719+atharvakumare03@users.noreply.github.com>
2025-02-20 12:14:34 +0530

    Create warehouse.java

---
+++ Q5/warehouse.java
@@ -0,0 +1,24 @@
+package Q5;
+
+import java.util.Arrays;
+
+public class warehouse{
+    public static void moveZeros(int[] crates) {
+        int index = 0; 
+        for (int num : crates) {
+            if (num != 0) {
+                crates[index++] = num;
+            }
+        }
+        while (index < crates.length) {
+            crates[index++] = 0;
+        }
+    }
+    public static void main(String[] args) {
+        int[] crates = {3,0,2,1,3,0};
+
+        System.out.println("Crate order before sorting: " + Arrays.toString(crates));
+        moveZeros(crates);
+        System.out.println("Final arrangement of crates: " + Arrays.toString(crates));
+    }
+}
